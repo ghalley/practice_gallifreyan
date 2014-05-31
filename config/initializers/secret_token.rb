1f3bf5a72e6caec943798b74d625ccab2965dc7a
Gabriel Halley <gabhalley@gmail.com>
2014-05-31 15:39:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeGallifreyan::Application.config.secret_key_base = '16938cb3fb48f897ba561c2cca64f8bfa1d9b5223c657b024088c4bcb0e1f6ab3346c1c4e7c3c1d03aef8666ac4166228802cc2556b8c466c7a2882c5243f29d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PracticeGallifreyan::Application.config.session_store :cookie_store, key: '_practice_gallifreyan_session'
